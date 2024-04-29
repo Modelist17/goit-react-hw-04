28b4f2b3082daad431bf9113a903a8bf944e8dba
Modelist17 <hunteruboat@gmail.com>
2024-04-29 10:27:29 +0200

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
-const ACCESS_KEY = "Client-ID 0RMlVjSgfmfRLcy228KXwjhyYTOg24ox1A18PLKzFEA";
-
-const searchParms = {
-  Authorization: ACCESS_KEY,
-  query: "",
-  page: 1,
-  per_page: 12,
-  orientation: "landscape",
-};
+const ACCESS_KEY = "0RMlVjSgfmfRLcy228KXwjhyYTOg24ox1A18PLKzFEA"; 
 
 const searchImages = async (query, page) => {
-  const { data } = await axios.get("/search/photos", {
-    params: {
-      ...searchParms,
-      page,
-      query,
-    },
-  });
-  return data;
+  try {
+    const response = await axios.get("/search/photos", {
+      params: {
+        query,
+        page,
+        per_page: 12,
+        orientation: "landscape",
+      },
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`, 
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default searchImages;
